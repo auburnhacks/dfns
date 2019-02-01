d544388277a2aef314674c6601e8644fcbe8a67c
Sai Kiran Dasika <kirandasika30@gmail.com>
2019-02-01 07:14:59 -0600

    added new events

--- events/events.go
+++ events/events.go
@@ -16,9 +16,23 @@ var (
 	allEvents = []event{
 		// eventId user-link is a special event where we link a user to
 		// a random number
-		{EventID: "user-link", DisplayName: "HackerLink"},
 		{EventID: "check-in", DisplayName: "Hacker Check-in"},
+		{EventID: "user-link", DisplayName: "HackerLink"},
 		{EventID: "sat-lunch", DisplayName: "Saturday Lunch"},
+		{EventID: "sat-dinner", DisplayName: "Saturday Dinner"},
+		{EventID: "midnight-snack", DisplayName: "Midnight Snack"},
+		{EventID: "sun-breakfast", DisplayName: "Sunday BreakFast"},
+		{EventID: "sun-lunch", DisplayName: "Sunday Lunch"},
+		{EventID: "techtalk-1", DisplayName: "1:30pm Tech Talk"},
+		{EventID: "techtalk-2", DisplayName: "5:00pm Tech Talk"},
+		{EventID: "techtalk-3", DisplayName: "7:30pm Tech Talk"},
+		{EventID: "techtalk-4", DisplayName: "9:00pm Tech Talk"},
+		{EventID: "techtalk-5", DisplayName: "11:00pm Tech Talk"},
+		{EventID: "techtalk-6", DisplayName: "10:00am Tech Talk"},
+		{EventID: "mc1", DisplayName: "Mini Challenge #1"},
+		{EventID: "mc2", DisplayName: "Mini Challenge #2"},
+		{EventID: "mc3", DisplayName: "Mini Challenge #3"},
+		{EventID: "mc4", DisplayName: "Mini Challenge: Tech Trivia Contest"},
 	}
 )
 
